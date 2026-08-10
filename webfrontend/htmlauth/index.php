46a03ad8b485d4d2c03a9ad605fee0b28b2b55b0
tim_anders22 <timanders22@users.noreply.github.com>
2026-08-10 17:17:15 +0200

    Hilfe uebersetzbar: Text raus aus help.html, rein in help_de.ini und help_en.ini
    Die Hilfeseite trug ihren Text bisher fest verdrahtet in help.html -
    auf Deutsch. Wer das Plugin auf Englisch benutzte, bekam die Hilfe
    trotzdem auf Deutsch; uebersetzen liess sie sich nur, indem man die
    Datei kopierte und danach zwei Fassungen auseinanderlaufen liess.
    
    Jetzt enthaelt help.html nur noch Platzhalter <TMPL_VAR HILFE.Kxx>.
    Der Text steht in templates/lang/help_de.ini und help_en.ini.
    LBWeb::gethelp() leitet den Namen der Sprachdatei aus dem Namen der
    Hilfedatei ab und sucht sie in templates/lang/ - nicht neben der
    Hilfe. Deshalb liegen sie dort und nicht in templates/help/.
    
    Nachgeprueft: 30 Platzhalter, und zu jedem gibt es in BEIDEN
    Sprachdateien einen Wert. Der Abgleich lief ueber alle 42 Plugins;
    ein fehlender Schluessel haette eine leere Stelle in der Hilfe
    hinterlassen, ohne dass irgendetwas gemeldet worden waere.
    
    Nebenbei: der Speichern-Knopf trug die Farbe fuers Lesen (gruen)
    statt fuer eine Aktion (orange). Die Legende darueber sagte das
    Richtige, der Knopf nicht.
    
    Fassung 1.0.5 in plugin.cfg, release.cfg und prerelease.cfg;
    ARCHIVEURL und INFOURL auf den Tag v1.0.5.

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -175,7 +175,7 @@ if (is_file($mw_logfile)) {
 function mw_e($s) { return htmlspecialchars((string) $s, ENT_QUOTES, 'UTF-8'); }
 
 $mw_frame = class_exists('LBWeb', false);
-if ($mw_frame) { LBWeb::lbheader('Rasenm&auml;her (Robonect)', 'https://wiki.loxberry.de/', ''); }
+if ($mw_frame) { LBWeb::lbheader('Rasenm&auml;her (Robonect)', 'https://wiki.loxberry.de/', 'help.html'); }
 $mw_host = mw_e(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '<loxberry-ip>');
 ?>
 <style>
@@ -465,7 +465,7 @@ $mw_reiter = array(
 <div class="sm-knopfreihe sm-b-aktion">
   <form method="post" action="index.php">
     <input data-role="none" type="hidden" name="activetab" value="tab-loxone">
-    <button data-role="none" type="submit" name="token_neu" value="1">Neues Token erzeugen</button>
+    <button data-role="none" type="submit" name="token_neu" value="1"><?php echo mo_t('TEXT.K_TOKEN_NEU'); ?></button>
   </form>
 </div>
 <div class="sm-legende">
